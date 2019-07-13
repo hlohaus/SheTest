0ffbd3507cb42efaca92722d8b342b70623c6f80
Heiner Lohaus <h.lohaus@she-business.com>
2019-07-13 19:41:44 +0200

    Add kernel events

--- Subscribers/ConsoleEvents.php
+++ Subscribers/ConsoleEvents.php
@@ -5,10 +5,12 @@ namespace SheTest\Subscribers;
 use Symfony\Component\Console\ConsoleEvents as SymfonyConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class ConsoleEvents implements EventSubscriberInterface
 {
-
     /**
      * Returns an array of event names this subscriber wants to listen to.
      *
@@ -34,7 +36,8 @@ class ConsoleEvents implements EventSubscriberInterface
             //SymfonyConsoleEvents::COMMAND => 'onCommand',
             //SymfonyConsoleEvents::ERROR => 'onError',
             //SymfonyConsoleEvents::EXCEPTION => 'onException',
-            strtolower('Shopware_Command_Before_Run') => 'onRun'
+            strtolower('Shopware_Command_Before_Run') => 'onRun',
+            KernelEvents::REQUEST => 'onRequest'
         ];
     }
 
@@ -48,5 +51,16 @@ class ConsoleEvents implements EventSubscriberInterface
     {
         var_dump('On run:');
         var_dump($args->get('command')->getName());
+    }
+
+    public function onRequest(GetResponseEvent $event)
+    {
+        if($event->getRequest()->getPathInfo() === '/hello/') {
+            $event->getRequest()->attributes->set('_controller', function () {
+                return new Response('Hello WORLD');
+            });
+            // or
+            $event->setResponse(new Response('Hello WORLD'));
+        }
     }
 }
